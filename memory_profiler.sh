0f7546ac8aa6db39875a9cc68a0d1abbe60055e6
Elfsong <dumingzhe@126.com>
2025-01-09 16:36:35 +0000

    Memory Profiler

---
+++ memory_profiler.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Usage
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 <command> [args...]"
+  exit 1
+fi
+
+# Save commands as an array
+CMD=("$@")
+
+LOG_FILE="mem_usage.log"
+echo "timestamp_ns RSS_kB" > "$LOG_FILE"
+
+# Get PID
+"${CMD[@]}" &
+PID=$!
+
+while kill -0 "$PID" 2>/dev/null; do
+    timestamp_ns=$(date +%s%N)
+    
+    rss_kb=$(awk '/VmRSS/{print $2}' /proc/$PID/status 2>/dev/null)
+    if [ -z "$rss_kb" ]; then
+        rss_kb=0
+    fi
+    
+    echo "$timestamp_ns $rss_kb" >> "$LOG_FILE"
+    sleep 0.0001
+done
+
+# 5. Wait PID finish
+wait $PID 2>/dev/null
+
+echo "[+] Process $PID has finished. Memory usage log is in $LOG_FILE."
